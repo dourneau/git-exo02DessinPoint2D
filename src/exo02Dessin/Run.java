bb4251ddd13de4ba7977c19cbfe934f4f575ef7a
Didier <didier.dourneau@yahoo.com>
2021-10-15 14:21:16 +0200

    commit 06 - Exo04

--- src/exo02Dessin/Run.java
+++ src/exo02Dessin/Run.java
@@ -5,9 +5,10 @@ public class Run {
 	public static void main(String[] args) {
 		
 		
-
-		Point2DExo03 p1 = new Point2DExo03(5,7);
-		Point2DExo03 p2 = new Point2DExo03(-5,-2);
+		Point2DExo04 p1 = new Point2DExo04();
+		p1.x = 5;
+		p1.y = 7;
+		Point2DExo04 p2 = new Point2DExo04(-5,-2);
 		
 		System.out.println(p1);
 		int dX = 5;
@@ -26,6 +27,28 @@ public class Run {
 		
 		
 		
+		// Run Exo03
+        /*
+		Point2DExo03 p1 = new Point2DExo03(5,7);
+		Point2DExo03 p2 = new Point2DExo03(-5,-2);
+		
+		System.out.println(p1);
+		int dX = 5;
+		int dY = 2;
+		System.out.println("Translation de vecteur : ("+dX+','+dY+")");
+		p1.translater(dX,dY);
+		System.out.println(p1);
+		
+		System.out.println(p2);
+		dX = -5;
+		dY = -2;
+		System.out.println("Translation de vecteur : ("+dX+','+dY+")");
+		p2.translater(dX, dY);
+		System.out.println(p2);
+		*/
+		
+		
+		//Run Exo02
 		/*
 		Point2D p1 = new Point2D();
 		Point2D p2 = new Point2D();
